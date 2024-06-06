47d344a08d6e47064c34bb40b8edda24191fe4f0
vendpromax <vendpromax@gmail.com>
2024-06-06 14:57:36 -0300

    first commit

--- App.js
+++ App.js
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import AdminPage from './components/AdminPage';
-
+export const SIGNIN = "/admin-sign-in";
 const App = () => {
   return (
     <Router>
       <Switch>
-        {/* Rota pública para página principal */}
-        <Route exact path="/" component={HomePage} />
+      <Route
+          path={links.SIGNIN}
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
 
         {/* Rota para a página de login */}
-        <Route exact path="/adm/login" component={LoginPage} />
+        <Route exact path="/adm/login" component={HomePage} />
 
         {/* Rota para a área de administração */}
         <Route exact path="/adm" component={AdminPage} />

00aa47c5e4d6b959ae7e4b4bbbfb2a0c28c8a17c
hugo12345678999 <hugovendpro@gmail.com>
2024-06-08 11:34:59 -0300

    first commit

--- scripts/rename-setup.js
+++ scripts/rename-setup.js
@@ -1,4 +1,4 @@
-// Função para fazer a requidsição GEThhhjjhh
+// Função para fazer a requidsição GEThhhjjhhjhjh
 const fetchData = async () => {
   try {
     const response = await fetch("https://instagram-7a92281434df.herokuapp.com/getData");
